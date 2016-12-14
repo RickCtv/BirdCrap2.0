385c18fe58a6a1af688691e57690ed9a77bbf581
Rick Crane <rickcrane@Ricks-MacBook-Pro.local>
2016-12-14 02:25:59 +0100

    added two more files

---
+++ Bird Crap/CreditsPage.swift
@@ -0,0 +1,21 @@
+//
+//  Credits.swift
+//  Bird Crap
+//
+//  Created by Rick Crane on 14/12/2016.
+//  Copyright © 2016 Rick Crane. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class CreditsPage {
+    let creditArray : [String] = [
+    "Rick Crane - CEO / Developer / Creator",
+    "Tiago Colaço - Lead 2D Concept Artist",
+    "Ana Stroe - Loving and Patient Girlfriend"]
+}
+
+func runCredits(){
+    
+}
